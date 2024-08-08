16ed988e3d08e6658b8aa42c2999884f5c197c98
Ksenia Sitnikova <xenyasitnikova88@gmail.com>
2024-08-08 10:34:12 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+        n *= -1
+    }
+    const numberToString = String(n).split('')
+    const reversedArray = []
+    numberToString.forEach((item) => reversedArray.push(item))
+    if(reversedArray[0] == 0) {
+        reversedArray.shift() 
+    }
+    const reversedString = reversedArray.reverse().join('')
+    const stringToNumber = Number(reversedString)
+    return stringToNumber
 }
