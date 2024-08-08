e3caed54833e4dcd75fc51b4568960d22f1fb142
Ksenia Sitnikova <xenyasitnikova88@gmail.com>
2024-08-08 10:54:56 +0300

    fix: fix task method

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ module.exports = function reverse (n) {
     }
     const numberToString = String(n).split('')
     const reversedArray = []
-    numberToString.forEach((item) => reversedArray.push(item))
+    for (let i = numberToString.length - 1; i >= 0; i--) {
+        reversedArray.push(numberToString[i])
+    }
     if(reversedArray[0] == 0) {
         reversedArray.shift() 
     }
-    const reversedString = reversedArray.reverse().join('')
+    const reversedString = reversedArray.join('')
     const stringToNumber = Number(reversedString)
     return stringToNumber
 }
+
